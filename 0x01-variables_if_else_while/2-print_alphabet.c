af7e15c35999c252f30bf519f224e30ae5708b98
abeni-berhanu <abenezer.berhanu.gs@gmail>
2022-09-09 13:02:20 -0700

    msg

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* This is the comment I wrote in c for the first time */
+
+int main(void)
+
+{
+char small_letters;
+
+for (small_letters = 'a'; small_letters <= 'z'; small_letters++)
+
+{
+putchar(small_letters);
+}
+
+putchar('\n');
+
+return (0);
+
+}
