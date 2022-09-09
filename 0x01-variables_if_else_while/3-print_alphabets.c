fd7e3501bce3e56c42092fa22667e7cf9af4344c
abeni-berhanu <abenezer.berhanu.gs@gmail>
2022-09-09 13:23:40 -0700

    msg

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main-fun
+ * uppuer case and lower case
+ */
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
