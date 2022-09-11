ec0ff460580e7ae829f07b61bbcdecc795372ce6
abeni-berhanu <abenezer.berhanu.gs@gmail>
2022-09-11 12:52:58 -0700

    msg

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+prinf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
+
+return (0);
+
+}
