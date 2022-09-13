5e26f31796a1590fcafdd3b082ccff4d0be53fd6
Abenezer Berhanu <111008585+abeni-berhanu@users.noreply.github.com>
2022-09-14 00:30:22 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: n is an integer
+ * @c: c is an integer
  * Return: 1 (if positive), 0 (if zero), -1 (if negative)
  */
 int print_sign(int c)
