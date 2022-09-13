ddefa5170ebf8f5cbe9e4bfca2325753dcc5f9f7
Abenezer Berhanu <111008585+abeni-berhanu@users.noreply.github.com>
2022-09-13 22:40:44 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
 #include "main.h"
+
 /**
- *main - int
+ * main - prints _putchar
  *
- *Return: 0
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
+char text[9] = "_putchar";
+int i = 0;
 
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+for (i = 0; i < 8; i++)
+{
+_putchar(text[i]);
+}
 _putchar('\n');
 
 return (0);
-
 }
