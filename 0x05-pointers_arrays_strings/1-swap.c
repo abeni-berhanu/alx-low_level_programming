f89491c13ca544e7c096674d4796fb5a195fcc0a
abeni-berhanu <abenezer.berhanu.gs@gmail.com>
2022-09-20 00:38:14 +0300

    msg

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - swaps value of two variables
+ * @a - pointer 1
+ * @b -pointer 2
+ * @h -host
+ * return: 0
+ */
+void swap_int(int *a, int *b)
+{
+int h;
+
+h = *a;
+a = *b;
+b = h;
+
+}
